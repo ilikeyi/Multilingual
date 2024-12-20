01b1541bf4dd6faa73722c1058f613eb012af67c
iLikeYi <775159955@qq.com>
2024-12-20 14:52:34 +0800

    Latest

--- Engine/Modules/1.1.1.2/Functions/Page/Engine.HomePage.psm1
+++ Engine/Modules/1.1.1.2/Functions/Page/Engine.HomePage.psm1
@@ -85,10 +85,10 @@ Function Mainpage
 	Write-Host " H'elp * " -NoNewline -BackgroundColor DarkMagenta -ForegroundColor White
 	Write-Host " " -NoNewline
 
-	Write-Host " $($lang.Short_Cmd) " -NoNewline -BackgroundColor DarkMagenta -ForegroundColor White
+	Write-Host " $($lang.Short_Cmd) " -NoNewline -BackgroundColor White -ForegroundColor Black
 	Write-Host " " -NoNewline
 
-	Write-Host " $($lang.Options) " -NoNewline -BackgroundColor DarkMagenta -ForegroundColor White
+	Write-Host " $($lang.Options) " -NoNewline -BackgroundColor White -ForegroundColor Black
 	Write-Host ": " -NoNewline
 
 	switch -Wildcard (Read-Host)
